c673ff22ce85d826f1e3a98bf8e22beeac8f812b
vbergmann <vbergmann@783448d1-dfc0-4521-9cb0-58c191f7d5bb>
2012-06-21 09:05:32 +0000

    New utility class GUIUtil with method createFileChooser()
    git-svn-id: https://svn.code.sf.net/p/databenecommons/code/trunk@100 783448d1-dfc0-4521-9cb0-58c191f7d5bb

---
+++ src/main/java/org/databene/commons/ui/GUIUtil.java
@@ -0,0 +1,53 @@
+/*
+ * (c) Copyright 2010-2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.commons.ui;
+
+import java.io.File;
+
+import org.databene.commons.SystemInfo;
+import org.databene.commons.ui.FileChooser;
+import org.databene.commons.ui.FileOperation;
+import org.databene.commons.ui.FileTypeSupport;
+import org.databene.commons.ui.awt.AwtFileChooser;
+import org.databene.commons.ui.swing.SwingFileChooser;
+
+/**
+ * Provides GUI utility methods.<br/><br/>
+ * Created: 01.12.2010 13:55:23
+ * @since 0.2.4
+ * @author Volker Bergmann
+ */
+public class GUIUtil {
+
+	public static FileChooser createFileChooser(
+			File selectedFile, FileTypeSupport supportedTypes, FileOperation operation) {
+		FileChooser chooser;
+        if (SystemInfo.isMacOsx())
+        	chooser = new AwtFileChooser(null, operation, supportedTypes);
+        else
+	        chooser = new SwingFileChooser(supportedTypes, operation);
+        if (selectedFile != null && selectedFile.exists())
+            chooser.setSelectedFile(selectedFile);
+        return chooser;
+	}
+	
+}
