d7285482d03d5d9d9bdcc3b362c82dd9a6a9acc3
vbergmann <vbergmann@783448d1-dfc0-4521-9cb0-58c191f7d5bb>
2012-12-15 07:44:29 +0000

    New method ExceptionUtil.containsException()
    git-svn-id: https://svn.code.sf.net/p/databenecommons/code/trunk@191 783448d1-dfc0-4521-9cb0-58c191f7d5bb

--- src/main/java/org/databene/commons/ExceptionUtil.java
+++ src/main/java/org/databene/commons/ExceptionUtil.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2009 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2009-2012 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -42,4 +42,15 @@ public class ExceptionUtil {
 			result = result.getCause();
 		return result;
 	}
+	
+	/** Scans recursively through an Exception and its 'cause' chain and tells if a given Exception type is contained. */
+	public static boolean containsException(Class<? extends Throwable> exceptionType, Throwable exception) {
+		do {
+			if (exceptionType.isAssignableFrom(exception.getClass()))
+				return true;
+			exception = exception.getCause();
+		} while (exception != null);
+		return false;
+	}
+	
 }
