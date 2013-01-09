825c9ea50ed6e93071cffd053c380ddf2ab7b043
vbergmann <vbergmann@783448d1-dfc0-4521-9cb0-58c191f7d5bb>
2013-01-09 12:12:39 +0000

    Made ContextStack an interface, moved former implementation to SimpleContextStack and removed locks
    git-svn-id: https://svn.code.sf.net/p/databenecommons/code/trunk@211 783448d1-dfc0-4521-9cb0-58c191f7d5bb

--- src/main/java/org/databene/commons/context/ContextStack.java
+++ src/main/java/org/databene/commons/context/ContextStack.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2008-2012 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2008-2013 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -26,16 +26,7 @@
 
 package org.databene.commons.context;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
-import java.util.Map.Entry;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-import org.databene.commons.Assert;
 import org.databene.commons.Context;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * A Stack of Contexts, querying for items from top to bottom, 
@@ -43,120 +34,7 @@ import org.slf4j.LoggerFactory;
  * @since 0.3.0
  * @author Volker Bergmann
  */
-public class ContextStack implements Context {
-    
-    private static final Logger logger = LoggerFactory.getLogger(ContextStack.class);
-
-    protected Stack<Context> contexts;
-    protected ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
-    
-    public ContextStack(Context ... contexts) {
-        this.contexts = new Stack<Context>();
-        for (Context c : contexts) 
-            this.contexts.push(c);
-    }
-
-    @Override
-	public synchronized Object get(String key) {
-    	try {
-    		lock.readLock().lock();
-	        for (int i = contexts.size() - 1; i >= 0; i--) {
-	            Object result = contexts.get(i).get(key);
-	            if (result != null)
-	                return result;
-	        }
-	        return null;
-    	} finally {
-    		lock.readLock().unlock();
-    	}
-    }
-
-	@Override
-	public boolean contains(String key) {
-    	try {
-    		lock.readLock().lock();
-	        for (int i = contexts.size() - 1; i >= 0; i--) {
-	            Context c = contexts.get(i);
-	            if (c.contains(key))
-	            	return true;
-	        }
-	        return false;
-    	} finally {
-    		lock.readLock().unlock();
-    	}
-    }
-
-    @Override
-	public synchronized Set<String> keySet() {
-    	try {
-    		lock.readLock().lock();
-	        Set<String> keySet = new HashSet<String>();
-	        for (int i = contexts.size() - 1; i >= 0; i--) {
-	            Context c = contexts.get(i);
-	            keySet.addAll(c.keySet());
-	        }
-	        return keySet;
-    	} finally {
-    		lock.readLock().unlock();
-    	}
-    }
-
-	@Override
-	public Set<Entry<String, Object>> entrySet() {
-    	try {
-    		lock.readLock().lock();
-			Set<Entry<String, Object>> entrySet = new HashSet<Entry<String, Object>>();
-	        for (int i = 0; i < contexts.size(); i++) {
-	            Context c = contexts.get(i);
-	            entrySet.addAll(c.entrySet());
-	        }
-	        return entrySet;
-    	} finally {
-    		lock.readLock().unlock();
-    	}
-    }
-
-	@Override
-	public synchronized void remove(String key) {
-    	try {
-    		lock.writeLock().lock();
-	        if (contexts.size() > 0)
-	            contexts.peek().remove(key);
-    	} finally {
-    		lock.writeLock().unlock();
-    	}
-    }
-
-    @Override
-	public synchronized void set(String key, Object value) {
-    	try {
-    		lock.writeLock().lock();
-	    	Assert.notNull(key, "key");
-	        if (contexts.size() > 0)
-	            contexts.peek().set(key, value);
-	        else
-	            logger.warn("ContextStack is empty, ignoring element: " + key);
-    	} finally {
-    		lock.writeLock().unlock();
-    	}
-    }
-
-    public synchronized void push(Context context) {
-    	try {
-    		lock.writeLock().lock();
-    		this.contexts.push(context);
-    	} finally {
-    		lock.writeLock().unlock();
-    	}
-    }
-    
-    public synchronized Context pop() {
-    	try {
-    		lock.writeLock().lock();
-    		return this.contexts.pop();
-    	} finally {
-    		lock.writeLock().unlock();
-    	}
-    }
-
+public interface ContextStack extends Context {
+    void push(Context context);
+    Context pop();
 }
