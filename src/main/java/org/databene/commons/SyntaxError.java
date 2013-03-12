4eb47804291e4b0a6505070359095797b8160395
vbergmann <vbergmann@783448d1-dfc0-4521-9cb0-58c191f7d5bb>
2013-03-12 12:50:19 +0000

    Improved syntax error messaging
    git-svn-id: https://svn.code.sf.net/p/databenecommons/code/trunk@248 783448d1-dfc0-4521-9cb0-58c191f7d5bb

--- src/main/java/org/databene/commons/SyntaxError.java
+++ src/main/java/org/databene/commons/SyntaxError.java
@@ -32,7 +32,7 @@ public class SyntaxError extends ParseException {
 	private static final long serialVersionUID = 835847387636212632L;
 
 	public SyntaxError(String message, String parsedText) {
-		super(message, parsedText);
+		super(message + ": " + parsedText, parsedText);
 	}
 
 	public SyntaxError(String message, String parsedText, int line, int column) {
