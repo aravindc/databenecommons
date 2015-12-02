dbd654b76f3c88a11fc30f954e1fbd2b0b31692d
vbergmann <vbergmann@783448d1-dfc0-4521-9cb0-58c191f7d5bb>
2015-12-02 06:59:19 +0000

    XPathUtil: Simplified existing methods, cleaned up and generified interface
    git-svn-id: https://svn.code.sf.net/p/databenecommons/code/trunk@565 783448d1-dfc0-4521-9cb0-58c191f7d5bb

--- src/main/java/org/databene/commons/xml/XPathUtil.java
+++ src/main/java/org/databene/commons/xml/XPathUtil.java
@@ -22,8 +22,8 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /**
@@ -35,61 +35,48 @@ import org.w3c.dom.NodeList;
 
 public class XPathUtil {
 
-	public static List<Element> queryElements(Document document, String expression) throws XPathExpressionException {
-		return XMLUtil.toElementList(queryNodes(document, expression));
+	public static List<Element> queryElements(Node base, String expression) throws XPathExpressionException {
+		return XMLUtil.toElementList(queryNodes(base, expression));
 	}
 
-	public static List<Element> queryElements(Element element, String expression) throws XPathExpressionException {
-		return XMLUtil.toElementList((NodeList) query(element, expression, XPathConstants.NODESET));
+	public static Element queryElement(Node base, String expression) throws XPathExpressionException {
+		return (Element) query(base, expression, XPathConstants.NODE);
 	}
 
-	public static Element queryElement(Document document, String expression) throws XPathExpressionException {
-		return (Element) query(document, expression, XPathConstants.NODE);
-	}
-
-	public static String queryElementText(Document document, String elementExpression) throws XPathExpressionException {
+	public static String queryElementText(Node base, String elementExpression) throws XPathExpressionException {
 		// a direct string query for the content of a missing element would return an empty string, 
 		// not allowing to differ an empty element from a missing one, so I need to take a two-step approach:
-        Element element = queryElement(document, elementExpression);
+        Element element = queryElement(base, elementExpression);
         if (element == null)
         	return null;
 		return element.getTextContent();
 	}
 
-	public static String queryAttribute(Document document, String elementExpression, String attributeName) throws XPathExpressionException {
+	public static String queryAttribute(Node base, String elementExpression, String attributeName) throws XPathExpressionException {
 		// since the result of a string query does not differentiate between empty ("") and not found (null), I need to apply a workaround
 		// see http://stackoverflow.com/questions/17390684/jaxp-xpath-1-0-or-2-0-how-to-distinguish-empty-strings-from-non-existent-value
-        Element element = queryElement(document, elementExpression);
+        Element element = queryElement(base, elementExpression);
         if (element == null)
         	return null;
         NodeList nodes = queryNodes(element, "@" + attributeName);
 		return (nodes.getLength() == 0 ? null : nodes.item(0).getTextContent());
 	}
 	
-	public static String queryString(Document document, String expression) throws XPathExpressionException {
-		return (String) query(document, expression, XPathConstants.STRING);
+	public static String queryString(Node base, String expression) throws XPathExpressionException {
+		return (String) query(base, expression, XPathConstants.STRING);
 	}
 
-	public static String queryString(Element element, String expression) throws XPathExpressionException {
-		return (String) query(element, expression, XPathConstants.STRING);
+	public static NodeList queryNodes(Node base, String expression) throws XPathExpressionException {
+		return (NodeList) query(base, expression, XPathConstants.NODESET);
 	}
 
-	public static NodeList queryNodes(Document document, String expression) throws XPathExpressionException {
-		return (NodeList) query(document, expression, XPathConstants.NODESET);
+	public static Node queryNode(Node base, String expression) throws XPathExpressionException {
+		return (Node) query(base, expression, XPathConstants.NODE);
 	}
 
-	public static NodeList queryNodes(Element element, String expression) throws XPathExpressionException {
-		return (NodeList) query(element, expression, XPathConstants.NODESET);
-	}
-
-	public static Object query(Document document, String expression, QName returnType) throws XPathExpressionException {
+	public static Object query(Node base, String expression, QName returnType) throws XPathExpressionException {
 		XPath xpath = XPathFactory.newInstance().newXPath();
-		return xpath.evaluate(expression, document, returnType);
-	}
-	
-	public static Object query(Element element, String expression, QName returnType) throws XPathExpressionException {
-		XPath xpath = XPathFactory.newInstance().newXPath();
-		return xpath.evaluate(expression, element, returnType);
+		return xpath.evaluate(expression, base, returnType);
 	}
 
 }
