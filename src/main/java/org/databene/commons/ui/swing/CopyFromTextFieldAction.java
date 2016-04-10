fbfe0f2b46c928b05ffcf973270f4d978576d5c2
vbergmann <vbergmann@783448d1-dfc0-4521-9cb0-58c191f7d5bb>
2016-04-10 21:36:12 +0000

    Fixed license headers
    git-svn-id: https://svn.code.sf.net/p/databenecommons/code/trunk@599 783448d1-dfc0-4521-9cb0-58c191f7d5bb

--- src/main/java/org/databene/commons/ui/swing/CopyFromTextFieldAction.java
+++ src/main/java/org/databene/commons/ui/swing/CopyFromTextFieldAction.java
@@ -1,23 +1,16 @@
 /*
- * (c) Copyright 2016 by Volker Bergmann. All rights reserved. 
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the GNU Lesser General Public License (LGPL) version 2.1 
- * which accompanies this distribution, and is available at 
- * http://www.gnu.org/licenses/lgpl-2.1.html
+ * Copyright (C) 2004-2015 Volker Bergmann (volker.bergmann@bergmann-it.de).
+ * All rights reserved.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
- * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
- * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.databene.commons.ui.swing;
